421a13ab2f5da2379bf22e7823e873bda4356822
ChanduAllada438 <chanduallada61@gmail.com>
2025-12-27 12:08:29 +0530

    Update one.py

--- one.py
+++ one.py
@@ -1,2 +1,18 @@
-print("Hello World")
-print("second time modifications")
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello from Python</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>This HTML file was generated using Python.</p>
+</body>
+</html>
+"""
+
+# create / overwrite the file
+with open("hello.html", "w") as file:
+    file.write(html_content)
+
+print("HTML file created: hello.html")
